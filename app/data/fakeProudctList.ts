532ea48fd64b8ad3cef819c5d901dfd7e6c09a29
kevin083177 <kevin083177@gmail.com>
2024-11-30 17:10:38 +0800

    Home screen Done!(fake data)

---
+++ app/data/fakeProudctList.ts
@@ -0,0 +1,15 @@
+import { Product } from "../interface/Product";
+
+export const fake_products: Product[] = [
+    { id: 1, image: require('../../assets/fake/Hachiware.jpg'), title: 'Hachiware', price: 150 },
+    { id: 2, image: require('../../assets/fake/KuriManju.jpg'), title: 'KuriManju', price: 130 },
+    { id: 3, image: require('../../assets/fake/Momonga.jpg'), title: 'Momonga', price: 2000 },
+    { id: 4, image: require('../../assets/fake/Rakko.jpg'), title: 'Rakko', price: 800 },
+    { id: 5, image: require('../../assets/fake/Shisa.jpg'), title: 'Shisa', price: 750 },
+    { id: 6, image: require('../../assets/fake/SleepParty.jpg'), title: 'SleepParty', price: 199 },
+    { id: 7, image: require('../../assets/fake/Star.png'), title: 'Star', price: 160 },
+    { id: 8, image: require('../../assets/fake/Usagi.png'), title: 'Usagi', price: 299 },
+    { id: 9, image: require('../../assets/fake/Yoroi San1.jpg'), title: 'Yoroi San1', price: 590 },
+    { id: 10, image: require('../../assets/fake/Yoroi San2.jpg'), title: 'Yoroi San2', price: 300 },
+    { id: 11, image: require('../../assets/fake/Yoroi San3.jpg'), title: 'Yoroi San3', price: 180 },
+  ];
