2f88140cf1d7e7c55ada6d7ed64aa138255f73de
kevin083177 <kevin083177@gmail.com>
2024-12-20 22:50:29 +0800

    Modify name: Collection -> Favorite

--- frontend/app/navigation/ProfileStackNavigator.tsx
+++ frontend/app/navigation/ProfileStackNavigator.tsx
@@ -2,7 +2,6 @@ import { createStackNavigator, } from "@react-navigation/stack";
 import { ProfileStackParamList } from "./type";
 import ProfileScreen from "../tabs/profile";
 import SettingScreen from "../tabs/profile/settings";
-import CollectionScreen from "../tabs/profile/collection";
 import PendingScreen from "../tabs/profile/pending";
 import CompleteScreen from "../tabs/profile/completed";
 import EvaluateScreen from "../tabs/profile/evaluate";
@@ -10,6 +9,7 @@ import ReportScreen from "../tabs/profile/report";
 import SellerScreen from "../tabs/profile/seller";
 import EditScreen from "../tabs/profile/edit";
 import { SlideUpTransition } from "../hook/SwitchPageAnimation";
+import FavoriteScreen from "../tabs/profile/favorite";
 const ProfileStack = createStackNavigator<ProfileStackParamList>(); //personal settings
   // Profile Stack Navigator
 export function ProfileStackNavigator() {
@@ -17,7 +17,7 @@ export function ProfileStackNavigator() {
       <ProfileStack.Navigator screenOptions={({ route }) => ({cardStyleInterpolator: route.name === 'Index' ? undefined : SlideUpTransition, headerShown: false, gestureEnabled: true, gestureDirection: 'vertical'})}>
         <ProfileStack.Screen name="Index" component={ProfileScreen} />
         <ProfileStack.Screen name="Setting" component={SettingScreen} />
-        <ProfileStack.Screen name="Collection" component={CollectionScreen} />
+        <ProfileStack.Screen name="Favorite" component={FavoriteScreen} />
         <ProfileStack.Screen name="Pending" component={PendingScreen} />
         <ProfileStack.Screen name="Completed" component={CompleteScreen} />
         <ProfileStack.Screen name="Evaluate" component={EvaluateScreen} />
