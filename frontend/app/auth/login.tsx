1da775017a26fc6d775a7e04c0eb5a7529f23de5
kevin083177 <kevin083177@gmail.com>
2024-12-29 15:58:24 +0800

    Modify styles

--- frontend/app/auth/login.tsx
+++ frontend/app/auth/login.tsx
@@ -1,81 +1,121 @@
-import { StyleSheet, Text, View, TextInput, TouchableOpacity, Alert} from 'react-native';
+import { StyleSheet, Text, View, TextInput, TouchableOpacity, Alert, ImageBackground, Image} from 'react-native';
 import { NavigationProp, useNavigation } from '@react-navigation/native';
 import React, { useEffect, useState } from 'react';
 import { api } from '../../api/api';
 import { asyncPost } from '../../utils/fetch';
 import { AuthStackParamList, RootStackParamList } from '../navigation/type';
 import { saveUserId } from '../../utils/stroage';
+import { LoadingModal } from '../components/LoadingModal';
 export default function LoginScreen() {
   const AuthNavigation = useNavigation<NavigationProp<AuthStackParamList>>();
   const RootNavigation = useNavigation<NavigationProp<RootStackParamList>>();
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const [errorText, setErrorText] = useState<string>("");
   const handleLogin = async () => {
     if (email === "" || password === "") {
-      Alert.alert("請輸入帳號或密碼");
+      setErrorText("請輸入帳號或密碼");
       return;
     }
-    const response = await asyncPost(api.login, {
-      "email": email,
-      "password": password
-    })
-    if (response.status === 200) {
-      saveUserId(response.data.body._id);
-      RootNavigation.reset({ index: 0, routes: [{ name: 'Main' }] });
-    } else {
-      Alert.alert("帳號或密碼錯誤");
+    try {
+      setIsLoading(true);
+      const response = await asyncPost(api.login, {
+        "email": email,
+        "password": password
+      })
+      if (response.status === 200) {
+        saveUserId(response.data.body._id);
+        RootNavigation.reset({ index: 0, routes: [{ name: 'Main' }] });
+      } else if (response.status === 401 || response.status === 404){
+        setErrorText("帳號或密碼錯誤");
+      } else {
+        setErrorText("伺服器異常");
+      }
+    } catch (error) {
+      console.log("failed to login");
+    } finally {
+      setIsLoading(false);
     }
   };
   return (
-    <View style={styles.container}>
-      <View style={styles.logo}>
-        <Text style={styles.logoText}>登入</Text>
-      </View>
-      <View>
-        <TextInput
-          style={styles.input}
-          placeholder="電子郵件"
-          keyboardType="email-address"
-          autoCapitalize="none"
-          value={email}
-          onChangeText={(text) => setEmail(text)}
-        />
-        <TextInput
-          style={styles.input}
-          placeholder="密碼"
-          secureTextEntry
-          value={password}
-          onChangeText={(text) => setPassword(text)}
-        />
-        <TouchableOpacity style={styles.loginButton} onPress={handleLogin}>
-          <Text style={styles.loginButtonText}>登入</Text>
-        </TouchableOpacity>
-        <View style={styles.bottom}>
-          <TouchableOpacity style={styles.Button}>
-            <Text style={styles.signupLink} onPress={() => AuthNavigation.navigate('Forget')}>忘記密碼?</Text>
-          </TouchableOpacity>
-          <TouchableOpacity style={styles.Button}>
-            <Text style={styles.signupLink} onPress={() => AuthNavigation.navigate('Register')}>註冊帳號</Text>
-          </TouchableOpacity>
+    <>
+      <ImageBackground 
+        source={require('../../assets/Auth-Background.png')}
+        style={styles.backgroundImage}
+        resizeMode="cover"
+      >
+        <View style={styles.container}>
+            <Text style={styles.logoText}>登入</Text>
+          <View>
+            <TextInput
+              style={styles.input}
+              placeholder="電子郵件"
+              keyboardType="email-address"
+              autoCapitalize="none"
+              value={email}
+              onChangeText={(text) => setEmail(text)}
+            />
+            <TextInput
+              style={styles.input}
+              placeholder="密碼"
+              secureTextEntry
+              value={password}
+              onChangeText={(text) => setPassword(text)}
+            />
+            <TouchableOpacity style={styles.loginButton} onPress={handleLogin}>
+              <Text style={styles.loginButtonText}>登入</Text>
+            </TouchableOpacity>
+            <Text style={styles.errorText}>{errorText}</Text>
+            <View style={styles.bottom}>
+              <TouchableOpacity style={styles.Button}>
+                <Text style={styles.signupLink} onPress={() => AuthNavigation.navigate('Forget')}>忘記密碼?</Text>
+              </TouchableOpacity>
+              <TouchableOpacity style={styles.Button}>
+                <Text style={styles.signupLink} onPress={() => AuthNavigation.navigate('Register')}>註冊帳號</Text>
+              </TouchableOpacity>
+            </View>
+          </View>
         </View>
-      </View>
-    </View>
+      </ImageBackground>
+      <LoadingModal isLoading={isLoading} message={"登入中..."} />
+    </>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
+    backgroundColor: 'rgb(255, 255, 255)',
+    alignItems: 'center',
+    justifyContent: 'center',
+    padding: 32,
+    borderRadius: 24,
+    shadowColor: "#000",
+    shadowOpacity: 0.25,
+    width: '85%',
+    elevation: 5,
+  },
+  backgroundImage: {
     flex: 1,
-    backgroundColor: '#fff',
+    width: '100%',
     alignItems: 'center',
     justifyContent: 'center',
   },
   logo: {
+    width: 100,
+    height: 100,
     marginBottom: 40,
   },
   logoText: {
     fontSize: 24,
     fontWeight: 'bold',
+    marginBottom: 40,
+  },
+  errorText: {
+    width: 200,
+    textAlign: 'center',
+    color: 'red',
+    marginBottom: 12,
   },
   form: {
     width: '80%',
