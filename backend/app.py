e9373bff73d9715153cb1021a09062e279f0d27b
kevin083177 <kevin083177@gmail.com>
2024-12-29 15:58:06 +0800

    Fix notification routes

--- backend/app.py
+++ backend/app.py
@@ -4,6 +4,7 @@ from config import Config, init_db
 from routes.product_routes import product_bp
 from routes.user_routes import user_bp
 from routes.order_routes import order_bp 
+from routes.notification_routes import notification_bp
 # from routes.test import test_bp
 
 app = Flask(__name__)
@@ -19,7 +20,7 @@ app.config["MongoDB"] = init_db()
 app.register_blueprint(product_bp, url_prefix="/products")
 app.register_blueprint(user_bp, url_prefix="/users")
 app.register_blueprint(order_bp, url_prefix="/orders")
-app.register_blueprint(order_bp, url_prefix="/notifications")
+app.register_blueprint(notification_bp, url_prefix="/notifications")
 # app.register_blueprint(test_bp, url_prefix="/test")
 
 if __name__ == "__main__":
